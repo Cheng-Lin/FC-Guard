56f8e2e933ad6f0d0ea3e85a1d7034703aae9f8a
Cheng Lin <clin684@gmail.com>
2016-05-26 07:52:39 -0400

    InterfaceTools: Added ArguementParser and a method for asking confirmation from user

--- util/InterfaceTools.cpp
+++ util/InterfaceTools.cpp
@@ -1,9 +1,28 @@
 #include "InterfaceTools.hpp"
 
+#include <boost/program_options.hpp>
+#include <cstdlib>
 #include <curses.h>
+#include <iostream>
 #include <string>
 
 using namespace std;
+namespace po = boost::program_options;
+
+const string InterfaceTools::DELETE_FILE = "delete-file";
+const string InterfaceTools::HELP = "help";
+const string InterfaceTools::FORCE = "force";
+const string InterfaceTools::INPUT = "input";
+const string InterfaceTools::RECURSIVE = "recursive";
+const string InterfaceTools::RE_ENCRYPT = "re-encrypt";
+
+bool InterfaceTools::GetConfirmation(const string &prompt)
+{
+    cout << prompt << " [y/n] ";
+    string line;
+    getline(cin, line);
+	return (line.compare("y") == 0) || (line.compare("Y") == 0);
+}
 
 string InterfaceTools::GetPassword(const char *prompt)
 {
@@ -18,3 +37,37 @@ string InterfaceTools::GetPassword(const char *prompt)
     endwin();
     return buff;
 }
+
+po::variables_map InterfaceTools::ParseArguement(int argc, const char* argv[])
+{
+    try
+    {
+        po::options_description description("Encyption/Decryption Options");
+        description.add_options()
+                ((InterfaceTools::HELP + ",h").c_str(), "Help Screen")
+                ((InterfaceTools::FORCE + ",f").c_str(), "Ignore all warnings")
+                ((InterfaceTools::INPUT + ",i").c_str(), po::value<string>()->required(),
+                        "Input can either be a file or directory")
+                ((InterfaceTools::DELETE_FILE + ",d").c_str(), "Keep original file")
+                ((InterfaceTools::RECURSIVE + ",r").c_str(), "Only works with directory, "
+                        "all subdirectories will be traversed")
+                (InterfaceTools::RE_ENCRYPT.c_str(), "Encrypt the encrypted file again");
+
+        po::variables_map vm;
+        po::store(po::command_line_parser(argc, argv).options(description).run(), vm);
+
+        if (vm.count(InterfaceTools::HELP)) {
+            cout << description << endl;
+            exit(0);
+        }
+
+        po::notify(vm);
+        return vm;
+    }
+    catch (const po::error &e)
+    {
+        cerr << e.what() << endl;
+        exit(1);
+    }
+}
+
--- util/InterfaceTools.hpp
+++ util/InterfaceTools.hpp
@@ -1,12 +1,22 @@
 #ifndef INTERFACE_TOOLS_HPP__
 #define INTERFACE_TOOLS_HPP__
 
+#include <boost/program_options.hpp>
 #include <string>
 
 class InterfaceTools
 {
     public:
+        static bool GetConfirmation(const std::string &prompt);
         static std::string GetPassword(const char *prompt);
+        static boost::program_options::variables_map ParseArguement(int argc, const char* argv[]);
+
+        static const std::string DELETE_FILE;
+        static const std::string HELP;
+        static const std::string FORCE;
+        static const std::string INPUT;
+        static const std::string RECURSIVE;
+        static const std::string RE_ENCRYPT;
 };
 
 #endif
